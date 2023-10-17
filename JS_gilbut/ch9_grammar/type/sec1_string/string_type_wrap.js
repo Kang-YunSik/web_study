4596fd7966b5f85a9962ecc396852a5c9af267ea
kys <kang_ys_1@naver.com>
2023-10-17 10:02:45 +0900

    string 객체 선언과 따옴표가 포함된 경우

---
+++ JS_gilbut/ch9_grammar/type/sec1_string/string_type_wrap.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// string 자료형은 작은 따옴표 또는 큰 따옴표로 선언한다.
+let string1 = "작은 따옴표";
+let string2 = "큰 따옴표";
+console.log(string1);
+console.log(string2);
+
+// string 자료형 안에 따옴표가 필요한 경우, 서로 다른 따옴표로 선언한다.
+let strict3 = '그는 "천채라고" 말했다';
+let strict4 = "그는 '천채라고' 말했다";
+console.log(string3);
+console.log(string4);
