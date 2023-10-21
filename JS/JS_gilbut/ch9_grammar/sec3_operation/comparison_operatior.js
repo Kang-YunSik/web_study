ebf328013df5f581b48a8676027a2f3be1a4cf41
kys <kang_ys_1@naver.com>
2023-10-21 19:13:09 +0900

    비교 연산자

--- JS/JS_gilbut/ch9_grammar/sec3_operation/comparison_operatior.js
+++
@@ -1,7 +0,0 @@
-"use restrict";
-let a = 5; // 숫자 5
-let b = "5"; // 문자 5
-let c = "6"; // 문자 6
-console.log(a == b); // true. 값만 따짐
-console.log(a === b); // false. 값과 타입을 따짐
-console.log(b === c); // false. 값과 타입을 따짐
